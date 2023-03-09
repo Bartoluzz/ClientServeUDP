590d4cda921280f961cc03003271ac9bf5d3de8d
Bartoluzz <fabio.bartolini0304@gmail.com>
2023-03-09 20:02:25 +0100

    C/S UDP OOP

--- src/datagramudp/ServerUDP.java
+++ src/datagramudp/ServerUDP.java
@@ -1,64 +1,67 @@
-
 package datagramudp;
 
 import java.io.*;
 import java.net.*;
 
-
 public class ServerUDP {
 
-    //capire come farlo con la OOP
-    public void start(){
+    private DatagramSocket serverSocket;
+    private int porta = 8080;
+    private byte[] bufferIN = new byte[1024];
+    private InetAddress clientIP;
+    private int portaClient;
+
+    public ServerUDP() throws SocketException {
         try {
-            int porta = 8080;
-            byte[] bufferIN = new byte[1024];
-            byte[] bufferOUT = new byte[1024];
-            boolean attivo = true;
-            
-            DatagramSocket serverSocket = new DatagramSocket(porta);
-            System.out.println("[1]. Server attivo sulla porta " + porta);
-            
-            DatagramPacket receivePacket = new DatagramPacket(bufferIN, 0, bufferIN.length);
-            
-            while(attivo == true){
-                
-                //ricevo messaggi dal client
-                serverSocket.receive(receivePacket);
-                int numChar = receivePacket.getLength();
-                String ricevuto = new String(receivePacket.getData());
-                ricevuto = ricevuto.substring(0,numChar);
-                System.out.println("Ricevuto : " + ricevuto);
-                                
-                if(ricevuto.equals("fine")){
-                    attivo = false;
-                    System.out.println("Richeista di finire la connessione");
-                }
-                
-                
-                //ricavo le info del client
-                InetAddress IPClient = receivePacket.getAddress();
-                int portaClient = receivePacket.getPort();
-                 
-                //spedisco messaggi al client
-                String daSpedire = ricevuto;
-                bufferOUT = daSpedire.getBytes();
-                DatagramPacket sendPakcet = new DatagramPacket(bufferOUT,0,bufferOUT.length, IPClient,portaClient);
-                serverSocket.send(sendPakcet);
-            }
-            
-            serverSocket.close();
-            
-        } catch (SocketException err){
-            System.err.println("Errore nell'istanziare il socket");
-        } catch (IOException err){
-            System.err.println("Errore I/O");
+            this.serverSocket = new DatagramSocket(porta);
+            System.out.println("Server Attivo");
+        } catch (SocketException ex) {
+            System.out.println("Errore nell'avvio del server: " + ex.getMessage());
+            throw ex;
         }
     }
- 
-    public static void main(String[] args) throws IOException{
-        
-             ServerUDP server = new ServerUDP();
-             server.start();
-        
+
+    public String ricezione() throws IOException {
+
+        DatagramPacket receivePacket = new DatagramPacket(bufferIN, bufferIN.length);
+        this.serverSocket.receive(receivePacket);
+
+        clientIP = receivePacket.getAddress();
+        portaClient = receivePacket.getPort();
+        String ricevuto = new String(receivePacket.getData(), 0, receivePacket.getLength());
+        System.out.println("Ricevuto : " + ricevuto);
+
+        return ricevuto;
     }
-}   
+
+    public void invio() throws IOException {
+
+        String daSpedire = ricezione();
+
+        if (daSpedire.equals("fine")) {
+            this.serverSocket.close();
+            return;
+        }
+
+        byte[] bufferOUT = daSpedire.getBytes();
+        DatagramPacket sendPacket = new DatagramPacket(bufferOUT, bufferOUT.length, clientIP, portaClient);
+        this.serverSocket.send(sendPacket);
+
+    }
+
+    public static void main(String[] args) {
+
+        try {
+            ServerUDP server = new ServerUDP();
+
+            while (true) {
+                server.invio();
+            }
+        } catch (IOException ex) {
+            System.out.println("Chiusura del server " + ex.getMessage());
+        } catch (Exception ex) {
+            System.err.println("Errore" + ex.getMessage());
+        }
+
+    }
+}
